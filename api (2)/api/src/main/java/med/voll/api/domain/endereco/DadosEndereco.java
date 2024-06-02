17ad54e1e161290b2b7d9e07fe6a6348be0aa71f
Bruno Pires Ferreira Maia de Barros <128605472+BrunoPFmaia@users.noreply.github.com>
2024-06-02 20:11:10 -0300

    Add files via upload

---
+++ api (2)/api/src/main/java/med/voll/api/domain/endereco/DadosEndereco.java
@@ -0,0 +1,20 @@
+package med.voll.api.domain.endereco;
+
+import jakarta.validation.constraints.*;
+
+public record DadosEndereco(
+        @NotBlank
+        String logradouro,
+        @NotBlank
+        String bairro,
+        @NotBlank
+        @Pattern(regexp = "\\d{8}")
+        String cep,
+        @NotBlank
+        String cidade,
+        @NotBlank
+        String uf,
+        String complemento,
+        String numero) {
+
+}
---
+++ api (2)/api/src/main/java/med/voll/api/domain/paciente/DadosListagemPaciente.java
@@ -0,0 +1,14 @@
+package med.voll.api.domain.paciente;
+import med.voll.api.domain.endereco.Endereco;
+
+public record DadosListagemPaciente(Long id,
+                                    String nome,
+                                    String email,
+                                    Endereco endereco) {
+    public DadosListagemPaciente(Paciente paciente){
+        this(   paciente.getId(),
+                paciente.getNome(),
+                paciente.getEmail(),
+                paciente.getEndereco());
+    }
+}
